c4806200f35fd61538c358cbeea002f359a4baa3
Wes Novack <wes-novack@pluralsight.com>
2020-08-23 09:30:15 -0700

    make BACKUP_IMAGE a constant, make regex string raw to resolve pytest warning, add try/except to image_available()

--- packt/free_title.py
+++ packt/free_title.py
@@ -4,6 +4,8 @@ import requests
 import re
 import logging
 
+BACKUP_IMAGE = 'https://www.packtpub.com/media/wysiwyg/homepage_split_promo/freelearn_split_right.png'
+
 
 def main():
     headers = {'user-agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.75 Mobile Safari/537.36'}
@@ -51,7 +53,6 @@ def get_cdn_data(product_id, headers):
 
 
 def check_image_availability(image, shop_url, headers):
-    backup_image = 'https://www.packtpub.com/media/wysiwyg/homepage_split_promo/freelearn_split_right.png'
     if image_available(image):
         return image
     else:
@@ -59,14 +60,14 @@ def check_image_availability(image, shop_url, headers):
         logging.info("shop_url: {}".format(url))
         response = requests.get(url, headers)
         logging.debug("response: {}".format(response.text))
-        match = re.search('(?<=og\:image" content=").*(?=\")', str(response.text))
+        match = re.search(r'(?<=og\:image" content=").*(?=\")', str(response.text))
         logging.info("match: {}".format(match))
         if match:
             shop_image = match.group(0)
             logging.info("shop_image: {}".format(shop_image))
             if image_available(shop_image):
                 return shop_image
-        return backup_image
+        return BACKUP_IMAGE
 
 
 def get_second_cdn_image(product_id):
@@ -74,12 +75,15 @@ def get_second_cdn_image(product_id):
 
 
 def image_available(image):
-    r = requests.head(image)
-    if r.status_code == 200:
-        return True
-    else:
+    try:
+        r = requests.head(image, timeout=5)
+        if r.status_code == 200:
+            return True
+        else:
+            return False
+    except Exception as e:
+        logging.info("Exception: {}".format(e))
         return False
-
 
 if __name__ == '__main__':
     main()
